448864b8569c5a6cf87a4799871253207e3a1510
Lulka <73712980+YuliyaValova@users.noreply.github.com>
2022-05-31 00:14:29 +0300

    Update test_dag.py

--- airflow/test_dag.py
+++ airflow/test_dag.py
@@ -6,54 +6,73 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 import requests
 
-def sendFailed():
-    message = "Failed"
-    bot_token = '...'
-    bot_chatID = '...'
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+import requests
+
+def read_cred(file):
+    confs = ""
+    file1 = open(file, "r")
+
+    while True:
+        line = file1.readline()
+        if line:
+            confs += "--conf " + line.strip() + " "
+        else:
+            break
+
+    return confs
+    file1.close
+
+
+    
+def sendMessage(message, **kwargs):
+    bot_token = '5266020788:AAFwxyJt1vryO2yT1f0DWyRBGggSJoLzpcM'
+    bot_chatID = '663207258' 
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + message
     requests.get(send_text)
  
 with DAG(
-    dag_id='test1_5',
+    dag_id='ABash_5',
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
     tags=['example']
 ) as dag:
-
-  # Reading conf file (now it only reading, but not in use)
-    read_cred = BashOperator(
-    task_id='read_cred',
-    bash_command='while read line; do echo --conf $line; done < /mnt/c/conf/spark.txt'
-    )
    
-  
+    read_cred = PythonOperator(
+        task_id="read_cred",
+        provide_context=True,
+        python_callable=read_cred,
+        op_kwargs={"file": '/mnt/c/conf/spark.txt'}
+  )
+        
     send_failed = PythonOperator(
         task_id="send_failed", 
         trigger_rule= 'one_failed',
-        python_callable=sendFailed
+        provide_context=True,
+        python_callable=sendMessage,
+        op_kwargs={"message": 'Credentials are not valid - Job failed!'}
   )
+  
+    send_success = PythonOperator(
+        task_id="send_success",
+        provide_context=True,
+        python_callable=sendMessage,
+        op_kwargs={"message":  'Job completed!'}
+  )
+  
     
-    submit_job = SparkSubmitOperator(
+    submit_job = BashOperator(
         trigger_rule= 'one_success',
         task_id="Spark-app",
-        conn_id='spark_local',
-        name='spark-practice',
-        application="/mnt/c/Users/User/DataEngineer_SparkPractice/target/scala-2.12/sparkPractice-assembly-0.1.0-SNAPSHOT.jar",
-        conf={
-            'spark.path': 
-            'spark.access.key': 
-            'spark.secret.key': 
-            'spark.endpoint': 
-            'spark.source.url':
-            'spark.source.username': 
-            'spark.source.password': 
-            'spark.save.type': 
-            'spark.source': 
-            'spark.fileName': 
-            'spark.dbtable': 
-        },
-        java_class="Main",
+        bash_command='/mnt/d/spark/spark-3.1.3-bin-hadoop3.2/bin/spark-submit --master=local[*] ' + "{{ti.xcom_pull(task_ids='read_cred')}}" + ' --class Main /mnt/c/Users/User/DataEngineer_SparkPractice/target/scala-2.12/sparkPractice-assembly-0.1.0-SNAPSHOT.jar'
+        
     )
     
-    read_cred >> [submit_job, send_failed]
+    read_cred >> [submit_job, send_failed] 
+    submit_job >> send_success
