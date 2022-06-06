e6f8200ef8b5bedd7e242a5c58e196edb6fc093d
Lulka <73712980+YuliyaValova@users.noreply.github.com>
2022-06-06 15:35:03 +0300

    Update test_dag.py

--- airflow/test_dag.py
+++ airflow/test_dag.py
@@ -20,7 +20,7 @@ def read_cred(conf_json, **kwargs):
     password = param["spark.source.password"]
     host = param["spark.source.url"] + ":sslConnection=true;"
     os.system("airflow connections add 'my_db2' --conn-type 'jdbc' --conn-login '" + login + "' --conn-password '" + password + "' --conn-host '" + host + "' --conn-extra \'{\"extra__jdbc__drv_clsname\":\"com.ibm.db2.jcc.DB2Driver\",\"extra__jdbc__drv_path\":\"<PATH_TO_SPARK_PROJECT>/DataEngineer_SparkPractice/target/scala-2.12/sparkPractice-assembly-0.1.0-SNAPSHOT.jar\"}\'")
-    os.system("airflow variables set db2_conf '" + db_conf + "'")
+    os.system("airflow variables set secret '" + db_conf + "'")
    
 def check_connection():
     connection = JdbcHook(jdbc_conn_id="my_db2")
@@ -102,7 +102,7 @@ with DAG(
     create_table = BashOperator(
         trigger_rule = 'one_success',
         task_id="create_table",
-        bash_command='java -cp <PATH_TO_SCALA_PROJECT>/DataEngineer_ScalaPractice/target/scala-2.13/DataEngineer_ScalaPractice-assembly-0.1.0-SNAPSHOT.jar load.LoadStarter ' + Variable.get("db2_conf", default_var=None)  
+        bash_command='java -cp <PATH_TO_SCALA_PROJECT>/DataEngineer_ScalaPractice/target/scala-2.13/DataEngineer_ScalaPractice-assembly-0.1.0-SNAPSHOT.jar load.LoadStarter ' + Variable.get("secret", default_var="")  
     )
         
     read_cred >> check_connection >> [table_exists, send_failed]
